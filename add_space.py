c5f91d957596503528f4def339b7e03fd789c81f
Yanfei Zhang <yz1345@nyu.edu>
2014-06-25 16:02:57 -0400

    Separated as 3000 per batch

--- add_space.py
+++ add_space.py
@@ -1,6 +1,6 @@
 with open ("2MRS_NED_z042.txt", "r") as myfile:
-	data=myfile.read().replace('-', ' -')
-	data1=data.replace('+', ' +')
+	data=myfile.read().replace('-', ', -')
+	data1=data.replace('+', ', +')
 print data1
 
 f = open("test.txt", "w")
